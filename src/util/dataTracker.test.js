3457b3eb1715537ec9333848135cb500ef640adf
kate <dubhcait@live.ie>
2019-11-19 17:16:55 +0000

    fix test

--- src/util/dataTracker.test.js
+++ src/util/dataTracker.test.js
@@ -1,20 +1,20 @@
-import dataTracker from './dataTracker'
+import { dataTracker } from './dataTracker'
 
 const testDataTracker = new dataTracker;
-testDataTracker.insert([ 3, 4 , 5 , 10, 3, -2])
+testDataTracker.insert([3, 4, 5, 10, 3, -2])
 
 test("Test showMin", () => {
-    expect(testDataTracker.showMin()).toEqual(-2);
-  });
+  expect(testDataTracker.showMin()).toEqual(-2);
+});
 
-  test("Test showMax", () => {
-    expect(testDataTracker.showMax()).toEqual(10);
-  });
+test("Test showMax", () => {
+  expect(testDataTracker.showMax()).toEqual(10);
+});
 
-  test("Test showMean", () => {
-    expect(testDataTracker.showMean()).toEqual(3.83);
-  });
+test("Test showMean", () => {
+  expect(testDataTracker.showMean()).toEqual(3.83);
+});
 
-    test("Test showMode", () => {
-        expect(testDataTracker.showMode()).toEqual(3);
-  });
+test("Test showMode", () => {
+  expect(testDataTracker.showMode()).toEqual(3);
+});

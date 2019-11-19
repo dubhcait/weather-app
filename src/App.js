a983125f311a3d59b62f312f9067956e7a59c7d0
kate <dubhcait@live.ie>
2019-11-19 11:40:48 +0000

    add weather api query

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import Button from '@material-ui/core/Button';
+
+import axios from 'axios';
+
+import { baseURL, dataPoints } from './util/constants'
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  formControl: {
+    margin: theme.spacing(3),
+  },
+  // formControl: {
+  //   margin: theme.spacing(1),
+  //   minWidth: 120,
+  // },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
 function App() {
+
+  const [cityName, setCityName] = useState('');
+  const [units, setUnits] = useState('metric');
+  const [countryCode, setCountryCodes] = useState('');
+
+  const classes = useStyles();
+  const query = `http://${baseURL}?q=${cityName},${countryCode}&cnt=${dataPoints}&units=${units}&APPID=${process.env.REACT_APP_API_KEY}`
+  const onSubmit = () => { 
+    axios.get(query).then(response => console.log(response.data))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather forcast</h1>
+      <form className={classes.container} noValidate autoComplete="off">
+        <div>
+          <TextField
+            className={classes.textField}
+            value={cityName}
+            onChange={(e) => { setCityName(e.target.value) }}
+            label="City"
+            margin="normal"
+          />
+        </div>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-simple-select-label">Country</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={countryCode}
+            onChange={(e) => { setCountryCodes(e.target.value) }}
+          >
+            <MenuItem value={'ca'}>Canada</MenuItem>
+            <MenuItem value={'in'}>India</MenuItem>
+            <MenuItem value={'ie'}>Ireland</MenuItem>
+            <MenuItem value={'pl'}>Poland</MenuItem>
+            <MenuItem value={'gb'}>United Kingdom of Great Britain and Northern Ireland</MenuItem>
+            <MenuItem value={'us'}>United States of America</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl component="fieldset" className={classes.formControl}>
+          <FormLabel component="legend">Units</FormLabel>
+          <RadioGroup aria-label="gender" name="gender2" value={units} onChange={(e) => { setUnits(e.target.value) }}>
+            <FormControlLabel
+              value="metric"
+              control={<Radio color="primary" />}
+              label="metric"
+              labelPlacement="start"
+            />
+            <FormControlLabel
+              value="imperial"
+              control={<Radio color="primary" />}
+              label="imperial"
+              labelPlacement="start"
+            />
+          </RadioGroup>
+          <Button variant="contained" color="primary" onClick={onSubmit} className={classes.button}>
+            Get Forcast
+      </Button>
+        </FormControl>
+      </form>
     </div>
   );
 }
---
+++ src/util/constants.js
@@ -0,0 +1,5 @@
+const baseURL = "api.openweathermap.org/data/2.5/forecast";
+const dataPoints = 5;
+
+
+export { baseURL, dataPoints}
